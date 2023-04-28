aa74f1cd9a3a506a65db5ae9968e6d22ad7802a6
ThidarPhyo <thidarphyo22031996@gmail.com>
2023-04-29 00:44:33 +0900

    Initial Commit

---
+++ WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by Thidar Phyo on 4/29/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
